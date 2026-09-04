ffdc681ee7dfcbfe62c2b8d92a111f4e4d7638f9
Rasika Bhalerao <rasikabh@gmail.com>
2026-09-04 04:06:14 -0700

    handout code should fail most tests

--- tests/test_q1.py
+++ tests/test_q1.py
@@ -1,18 +1,18 @@
 """HW1 Question 1 Tests"""
 
-ORIGINAL_CONTENT = """\"\"\"HW1 Question 1\"\"\"
+ORIGINAL_CONTENT = '''"""HW1 Question 1
 
-\"\"\"To get full points on this question, modify this file and make a submission.
+To get full points on this question, modify this file and make a submission.
 The file must still work as a valid Python file.
 You can write a poem, write Python code, or just add a single character.
 
 Getting your setup ready is the hardest part of the semester.
 This question is designed to give you points for getting your GitHub and 
-Pawtograder setup working.\"\"\"
+Pawtograder setup working."""
 
-def print_meow():
-    \"\"\"Prints 'meow' to the console.\"\"\"
-    print("meow")"""
+def print_meow() -> None:
+    """Prints 'meow' to the console."""
+    print("meow")'''
 
 
 def test_q1_contents():
